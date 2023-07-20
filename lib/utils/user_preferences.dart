250ed5850f3c334067c7b40b45d13544920a7ed0
Neta4302 <54069403+Neta4302@users.noreply.github.com>
2023-07-20 15:43:21 +0700

    Add files via upload

---
+++ lib/utils/user_preferences.dart
@@ -0,0 +1,13 @@
+import 'package:project_1/model/user.dart';
+
+class UserPreferences {
+  static const myUser = User(
+    imagePath:
+        'https://images.unsplash.com/photo-1517423440428-a5a00ad493e8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    name: 'Nattaphat Sangsunt',
+    email: 's6407012660193@email.kmutnb.ac.th',
+    about:
+        'I am a junior at King Mongkut University of Technology North Bangkok. Now, I am learning and practicing web design and web development skills. Using my knowledge of information technology, I can do assigned tasks with self-training, research, and studying at the university stimulus.',
+    isDarkMode: false,
+  );
+}
